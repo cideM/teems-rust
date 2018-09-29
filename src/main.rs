dc3ca1550cfa13152aedc2a3049955cf243c2b02
Florian Beeres <fbeeres@gmail.com>
2018-09-30 00:52:47 +0200

    activate and list scaffold

--- src/main.rs
+++ src/main.rs
@@ -5,63 +5,185 @@ extern crate serde;
 extern crate serde_json;
 
 use clap::{App, Arg, SubCommand};
+use std::collections::HashMap;
+use std::convert::AsRef;
+use std::fmt;
 use std::fs;
-
-#[derive(Serialize, Deserialize, Debug)]
-struct ThemeColors {
-    color0: String,
-    color1: String,
-    color2: String,
-    color3: String,
-    color4: String,
-    color5: String,
-    color6: String,
-    color7: String,
-    color8: String,
-    color9: String,
-    color10: String,
-    color11: String,
-    color12: String,
-    color13: String,
-    color14: String,
-    color15: String,
-    background: String,
-    foreground: String,
-}
+use std::process;
 
 #[derive(Serialize, Deserialize, Debug)]
 struct Theme {
     name: String,
-    colors: ThemeColors,
+    colors: HashMap<String, String>,
+}
+
+impl fmt::Display for Theme {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let mut output = String::new();
+
+        output.push_str(&format!("Name: {}\n", &self.name));
+        output.push_str("Colors:\n");
+
+        for (color, value) in &self.colors {
+            output.push_str(&format!("\t0: {}: {}", color, value));
+        }
+
+        output.push_str("\n");
+        output.push_str("\n");
+
+        write!(f, "{}", output)
+    }
+}
+
+#[derive(Debug, Clone)]
+struct TeemsError {
+    msg: String,
+}
+
+impl TeemsError {
+    fn new(msg: &str) -> TeemsError {
+        TeemsError {
+            msg: msg.to_string(),
+        }
+    }
+}
+
+impl fmt::Display for TeemsError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "Error: {}", self.msg)
+    }
 }
 
 type Config = Vec<Theme>;
 
+struct Dispatcher {
+    apps: Vec<Box<Replacer>>,
+}
+
+impl Dispatcher {
+    fn run(&self, app_config: &str, theme: &Theme) {
+        for x in &self.apps {
+            x.replace(app_config, theme);
+        }
+    }
+}
+
+trait Replacer {
+    fn replace(&self, app_config: &str, theme: &Theme) -> Result<Vec<String>, TeemsError>;
+
+    fn name(&self) -> &str;
+
+    fn config_paths(&self) -> Vec<&str>;
+}
+
+struct Alacritty {
+    name: String,
+    config_paths: Vec<String>,
+}
+
+impl Alacritty {
+    fn new(name: &str, config_paths: Vec<String>) -> Alacritty {
+        Alacritty {
+            name: name.to_owned(),
+            config_paths,
+        }
+    }
+}
+
+impl Replacer for Alacritty {
+    fn replace(&self, app_config: &str, theme: &Theme) -> Result<Vec<String>, TeemsError> {
+        let mut result = Vec::new();
+
+        for path in &self.config_paths {
+            let content = match fs::read_to_string(path) {
+                Ok(x) => {
+                    result.push(x);
+                }
+                Err(e) => return Err(TeemsError::new(&format!("{}", e))),
+            };
+        }
+
+        Ok(result)
+    }
+
+    fn name(&self) -> &str {
+        &self.name
+    }
+
+    fn config_paths(&self) -> Vec<&str> {
+        self.config_paths.iter().map(AsRef::as_ref).collect()
+    }
+}
+
+fn list_themes(config: &Config) -> () {
+    for theme in config {
+        println!("{}", theme);
+    }
+}
+
 fn main() {
-    let matches = App::new("Teems")
+    // TODO: Initialize dispatcher and call run() in 'activate' branch
+    let alacritty = Alacritty::new("alacritty", vec![]);
+
+    let app = App::new("Teems")
         .version("0.1")
         .author("Florian B. <yuuki@protonmail.com")
         .about("Easily switch themes for your terminal(s)")
         .subcommand(SubCommand::with_name("list").about("List all themes"))
-        .args(&[
-            Arg::from_usage("-c, --config <FILE>, 'a required json file containing the themes'"),
-            // TODO: To stay consistent, maybe this should also be a subcommand?
-            Arg::from_usage("-t, --theme [THEME], 'an optional name of a theme'"),
-        ]).get_matches();
+        .subcommand(
+            SubCommand::with_name("activate")
+                .about("Activate a theme")
+                .arg(Arg::from_usage(
+                    "-t, --theme <THEME> 'a required name of a theme'",
+                )),
+        )
+        .args(&[Arg::from_usage(
+            "-c, --config <FILE> 'a required json file containing the themes'",
+        )]);
+
+    let matches = app.get_matches();
     // TODO: Add dry run flag
 
-    let config_path = matches.value_of("config").unwrap();
-    let config = fs::read_to_string(config_path);
+    let config_path = matches
+        .value_of("config")
+        .expect("Couldn't read '--config' value");
 
-    match config {
-        Ok(content) => {
-            // TODO: Exit with nice error message
-            let themes: Config = serde_json::from_str(&content).unwrap();
-            // TODO: Iterate over themes and print them nicely.
-            println!("{:?}", themes);
-        }
-        // TODO: This doesn't really help now does it? Maybe add a --debug flag which prints the error. Or just print
-        // the actual error after this message
-        Err(_) => println!("Error reading config file"),
+    let config_serialized = fs::read_to_string(config_path).expect("Error reading config file");
+    let config_deserialized = serde_json::from_str(&config_serialized);
+
+    match config_deserialized {
+        Ok(cfg) => match matches.subcommand() {
+            ("list", _) => {
+                list_themes(&cfg);
+            }
+            ("activate", Some(sub)) => {
+                let theme_name = sub
+                    .value_of("theme")
+                    .expect("Could not read 'theme' argument");
+
+                let theme = match cfg.into_iter().find(|x: &Theme| x.name == theme_name) {
+                    Some(t) => t,
+                    None => {
+                        eprintln!("Theme {} not found in config file", theme_name);
+                        process::exit(1);
+                    }
+                };
+
+                let result = match alacritty.replace("foo", &theme) {
+                    Ok(r) => r,
+                    Err(_) => {
+                        eprintln!("Error generating new app config");
+                        process::exit(1);
+                    }
+                };
+
+                println!("{:?}", result);
+            }
+            _ => {
+                // Default if no subcommand matched
+                list_themes(&cfg);
+            }
+        },
+        Err(_) => println!("Could not deserialize config file"),
     }
 }
